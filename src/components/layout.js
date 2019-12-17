1c50782a8f7c226117725ce6bb167c7c6d73097f
Narendra Kumar Vadapalli <narenandu@gmail.com>
2019-12-16 18:29:07 -0800

    Adding theme to the blog pages via blogTemplate

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,7 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql, Link} from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
@@ -35,6 +35,8 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
+        <hr></hr>
+        <Link to="/">Naren Home</Link>
         <footer>© {new Date().getFullYear()}, narenVadapalli</footer>
       </div>
     </>
---
+++ src/markdown-pages/my_webapp_techstack.md
@@ -0,0 +1,7 @@
+---
+path: "/blog/my-webapp-techstack"
+date: "2019-12-13"
+title: "Tech Stack for my Website"
+---
+
+This blog will be capturing the information about the tech stack used to create this website
