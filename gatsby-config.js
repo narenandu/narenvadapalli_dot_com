2754ee9d1f9318bdfb57dcb0e0796d07cac263e7
Narendra Kumar Vadapalli <narenandu@gmail.com>
2020-01-02 22:49:24 -0800

    Adding new blog for safely removing linux from dual boot  grub and efi

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,8 +16,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `markdown-pages`, 
-        path: `${__dirname}/src/markdown-pages`,
+        name: `blogs`, 
+        path: `${__dirname}/src/blogs`,
       },
     },
     `gatsby-transformer-remark`,
@@ -35,6 +35,21 @@ module.exports = {
         icon: `src/images/narenvadapalli_logo.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {
+        /*
+        - basePath defaults to `/`
+        - contentPath defaults to `content/posts`
+        - assetPath defaults to `content/assets`
+        - mdx defaults to `true`
+        */
+        basePath: `${__dirname}`,
+        contentPath: `src/blogs`,
+        //assetPath: `src/blogAssets`,
+        mdx: false,
+      },
+    },    
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -25,17 +25,6 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <div style={{position: `fixed`,
-              zIndex:`-99`,
-              width: `100%`,
-              height: `100%`,
-            }}
-      >
-        <iframe frameborder="0" height="100%" width="100%" 
-          src="https://youtube.com/embed/Dxs_xPKdr6E?autoplay=1&controls=0&showinfo=0&autohide=1&cc_load_policy=1">
-        </iframe>
-      </div>          
-
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
