3125f2e4d060d1d19fc409edfd2f0d69732f8216
Narendra Kumar Vadapalli <narenandu@gmail.com>
2020-10-19 22:51:31 -0700

    Removing deprecation warning by updating the pathContext to pageContext in gatsby context

--- src/templates/social_media_share_ribbon.js
+++ src/templates/social_media_share_ribbon.js
@@ -1,10 +1,24 @@
 import React from "react";
 import { TwitterIcon } from "react-share";
 
-export default function (props) {
+export default function(props) {
     return (
         <div>
-            <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-text="Content by @narenandu " data-url="https://www.narenvadapalli.com" data-show-count="true"><TwitterIcon size={64} round={true} /></a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-        </div >
-    )
+            <a
+                href="https://twitter.com/share?ref_src=twsrc%5Etfw"
+                class="twitter-share-button"
+                data-size="large"
+                data-text="Content by @narenandu "
+                data-url="https://www.narenvadapalli.com"
+                data-show-count="true"
+            >
+                <TwitterIcon size={64} round={true} />
+            </a>
+            <script
+                async
+                src="https://platform.twitter.com/widgets.js"
+                charset="utf-8"
+            ></script>
+        </div>
+    );
 }
